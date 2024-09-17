9f1a31d086c09088f6dcc30c6853cf266463e3dd
mbmoreinis <143663172+mbmoreinis@users.noreply.github.com>
2024-09-17 12:06:18 -0400

    Add files via upload

--- done.js
+++ done.js
@@ -3,6 +3,16 @@
 /* COLOR KATAS */
 
 /* No Prompt */
+// make a string variable for a color, set it equal to your favorite
+var color = "Red";
+// alert the message" "My favorite color is " plus the color
+alert("My favorite color is " + color);
+
+/* Var Alert Prompt */
+// make a string variable for color, prompt the user for their favorite
+color = prompt("What is yours?");
+// alert color " is a nice color!"
+alert(color + " is a nice color!");
 
 
 /* Prompt */
